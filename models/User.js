5993fbbf2dd0e31adacb107a56a5cd05d8713118
Stiven Mata <stivensifontesm@gmail.com>
2025-04-25 22:29:36 -0500

    agregue user

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+let collection = "users";
+
+let schema = new Schema(
+  {
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    photo: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let User = model(collection, schema);
+
+export default User;
